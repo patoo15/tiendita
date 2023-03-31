09fdc5d54f41f1b6ec0054065e21d75c4a90fd2e
Patricio Torres <patriciotorres1992@hotmail.com>
2023-03-30 23:27:30 -0300

    push

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h3>Ecommerce preEntrega1</h3>
+      <div>
+        <buttom>Laptos</buttom>
+        <buttom>Teclados</buttom>
+        <buttom>Celulares</buttom>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
